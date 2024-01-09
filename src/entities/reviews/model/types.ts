d01ad9768dc492206d5726a114c98e6792c9682d
Nikita Lomeiko <lomieiko78@mail.ru>
2024-01-10 00:42:43 +0300

    create json-server, add test-data, add entity city, add features sort-city,  fix ui, add ui errorMessage

---
+++ src/entities/reviews/model/types.ts
@@ -0,0 +1,7 @@
+export interface IReviewsScheme {
+    id: string,
+    idUser: string,
+    idExcursion: string,
+    score: number,
+    message: string,
+}
---
+++ src/entities/user/model/types.ts
@@ -0,0 +1,7 @@
+export interface IUserScheme {
+    id: string,
+    avatar: string,
+    name: string,
+    mail: string,
+    password: string,
+}
